83a5e2bcb4f378f1e260c03d8153d3ce1dce1bc1
Colton Davis <colt822lavay@outlook.com>
2018-12-08 09:41:29 -0700

    Create Models, Setup Routes, Start Rails Server

---
+++ db/migrate/20181208163501_create_addresses.rb
@@ -0,0 +1,14 @@
+class CreateAddresses < ActiveRecord::Migration[5.2]
+  def change
+    create_table :addresses do |t|
+      t.string :street
+      t.string :city
+      t.string :state
+      t.integer :zip
+      t.integer :location_id
+      t.belongs_to :location, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
