66b6a99bed2093e42f379826d7f81acee139b57a
Crys <sovereigncrys88@gmail.com>
2018-12-08 11:11:59 -0700

    added trip form

---
+++ client/src/components/TripForm.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import { Form, } from 'semantic-ui-react';
+
+class TripForm extends React.Component {
+        state = {
+        name: '',
+        start_date: '',
+        end_date: ''
+    };
+
+    handleChange = (e) => {
+        this.setState({ name: e.target.value });
+    };
+
+    handleSubmit = (e) => {
+        let { name, start_date, end_date } = this.state
+        e.preventDefault();
+        this.props.addTrip(name, start_date, end_date);
+        this.setState({
+            name: '',
+            start_date: '',
+            end_date: '',
+        })
+    };
+
+    render() {
+        let { name, start_date, end_date } 
+        return(
+            <Form onSubmit={this.handleSubmit}>
+                <Form.Input
+                    label="Trip Name"
+                    placeholder="Add A Trip Name"
+                    required
+                    value={this.state.name}
+                    onChange={this.handleChange}
+                />                
+                <Form.Input
+                    label="Trip Start"
+                    placeholder="Add Start Date"
+                    required
+                    value={this.state.name}
+                    onChange={this.handleChange}
+                />                    
+                <Form.Input
+                    label="Trip End"
+                    placeholder="Add End Date"
+                    required
+                    value={this.state.name}
+                    onChange={this.handleChange}
+                />
+            </Form>
+        )
+    }
+}
+            
+export default TripForm;
