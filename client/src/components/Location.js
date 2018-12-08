c3be5a2aaa2f8c1364a665826dd619984077808e
Matt Westover <matthew.westover@gmail.com>
2018-12-08 15:15:20 -0700

    add calls for address

--- client/src/components/Location.js
+++ client/src/components/Location.js
@@ -1,11 +1,51 @@
 import React from 'react';
 import LocationForm from './LocationForm';
+import Address from './Address'
+import axios from 'axios'
 import { Card, Button, Icon } from 'semantic-ui-react'
 
 class Location extends React.Component {
   state = { addresses: [], editing: false, }
 
-  toggleLocationEdit = () => this.setState({editing: !this.state.editing})
+  toggleAddressEdit = () => this.setState({editing: !this.state.editing})
+  
+  componentDidMount() {
+    let { id } = this.props
+    let { trip_id } = this.props.trip_id
+    axios.get(`/api/trips/${trip_id}/locations/${id}/addresses`)
+      .then(res => {
+        this.setState({ addresses: res.data, editing: false, })
+      })
+  }
+
+  addAddress = (id, trip_id, street, city, state, zip ) => {
+    axios.post(`/api/trips/${trip_id}/locations/${id}/addresses`, { street, city, state, zip })
+      .then( res => {
+        const { addresses } = this.state;
+        this.setState({ addresses: [...addresses, res.data], editing:false, })
+      })
+  }
+
+  updateAddress = ({id, trip_id, location_id,  street, city, state, zip}) => {
+    axios.put(`/api/trips/${trip_id}/locations/${location_id}/addresses/${id}`, { id, trip_id, location_id,  street, city, state, zip })
+    .then( res => {
+      const addresses = this.state.addresses.map( address => {
+      if (address.id === id)
+        return res.data;
+      return address;
+    });
+    this.setState({ addresses, editing: false, });
+  })
+  }
+
+  deleteLocation = (trip_id, id) => {
+    axios.delete(`/api/trips/${trip_id}/locations/${id}`)
+      .then(res => {
+        const { locations, } = this.state;
+        this.setState({ locations: locations.filter(t => t.id !== id) })
+      })
+  }
+
   render () {
     return (
       <Card.Content>
