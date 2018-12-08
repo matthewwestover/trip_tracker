781092bdf552674c66bd4c2d1033dfe744b644a5
Matt Westover <matthew.westover@gmail.com>
2018-12-08 10:00:24 -0700

    stubbed crud actions

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,62 @@
 import React, { Component } from 'react';
+import TripForm from './components/TripForm';
+import TripList from './components/TripList';
+import { Container, Divider } from 'semantic-ui-react';
 
 
 class App extends Component {
+  state = { trips: [], locations: [], addresses: [] }
+
+  componentDidMount () {
+
+  }
+
+  addTrip = (name, start_date, end_date) => {
+
+  }
+
+  updateTrip = () => {
+
+  }
+
+  deleteTrip = () => {
+
+  }
+
+  addLocation = () => {
+
+  }
+
+  updateLocation = () =>{
+
+  }
+
+  deleteLocation= () => {
+
+  }
+
+  addAddress = () => {
+
+  }
+
+  updateAddress = () => {
+
+  }
+
+  deleteAddress = () => {
+
+  }
+
+
   render() {
     return (
-      
+      <Container style={{ padding: "30px 0" }}>
+        <h1>Team Awesome's Trip Planner</h1>
+        <TripForm addTrip={this.addTrip} />
+        <Divider />
+        <br />
+        <TripList />
+      </Container>
     );
   }
 }
